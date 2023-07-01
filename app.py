d8dfbdce7287f9f2354990ecd2641100b757fdf2
muiruri7 <leon.w.muiruri@gmail.com>
2023-07-01 12:51:03 +0300

    Initial Commit

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import pandas as pd
 from config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB,UPLOAD_FOLDER, SECRET_KEY
 
 app = Flask(__name__)
+
 app.config['MYSQL_HOST'] = MYSQL_HOST
 app.config['MYSQL_USER'] = MYSQL_USER
 app.config['MYSQL_PASSWORD'] = MYSQL_PASSWORD
@@ -20,7 +21,6 @@ app.secret_key = SECRET_KEY
 mysql = MySQL(app)
 
 ALLOWED_EXTENSIONS = {'xlsx', 'xls'}
-UPLOAD_FOLDER = 'uploads/'
 
 # Defines!
 def retrieve_admin(username):
